8b86bf81a0809df51ee9eefca37555c5ac5aec1e
Alex Florea <alexflorea@alexs-mbp.myfiosgateway.com>
2023-03-15 19:22:37 -0400

    Done

--- create-remove.js
+++ create-remove.js
@@ -8,13 +8,30 @@ add.addEventListener("click", async () => {
         const url = data.message; // URL of new dog image
 
         /*--------------- Get breed (Hint: Parse from URL) ---------------- */
-        // Your code here
+        const breed = url.split('/')[4]
+        console.log(breed)
+       
 
         /*------------ Create new dog card with the url above ------------- */
         /* (use the HTML structure for the current dog image in the index.html
             file to create a new image with the url) */
-        // Your code here
+        const li = document.createElement('li')
 
+        li.innerHTML = `<figure>
+        <img src="${url}"/>
+        <figcaption>${breed}</figcaption>
+    </figure>`
+        // const figure = document.createElement('figure')
+        // const image = document.createElement('img')
+        // const figcaption = document.createElement('figcaption')
+        // image.setAttribute('src', url)
+        // figcaption.innerText = breed
+        // figure.appendChild(image)
+        // figure.appendChild(figcaption)
+        // li.appendChild(figure)
+
+        const ul = document.querySelector('ul')
+        ul.appendChild(li)
         /* Add the new dog card as a child to the ul in the .gallery element */
         // Your code here
 
@@ -27,8 +44,8 @@ add.addEventListener("click", async () => {
 const removeFirst = document.getElementById("remove-first");
 removeFirst.addEventListener("click", () => {
     /*-------------------- Select the first dog card --------------------- */
-    // Your code here
-
+    const first = document.querySelector('li')
+    first.remove()
     /*-------------------- Remove the first dog card --------------------- */
     // Your code here
 });
@@ -37,7 +54,10 @@ removeFirst.addEventListener("click", () => {
 const removeLast = document.getElementById("remove-last");
 removeLast.addEventListener("click", () => {
     /*-------------------- Select the last dog card ----------------------- */
-    // Your code here
+   const all = document.querySelectorAll('li')
+   console.log(all)
+   const last = all[all.length-1]
+   last.remove()
 
     /*-------------------- Remove the last dog card ----------------------- */
     // Your code here
